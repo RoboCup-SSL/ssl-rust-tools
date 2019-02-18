2748b6618d6556bf2ea22daaaff17a1e2c509cbb
Devin Schwab <digidevin@gmail.com>
2019-02-18 15:09:04 -0500

    :beginner: Example that prints log file to console

---
+++ examples/print_log.rs
@@ -0,0 +1,25 @@
+extern crate clap;
+extern crate ssl_log_tools;
+use clap::{App, Arg};
+use ssl_log_tools::persistence::reader;
+use std::path::Path;
+
+fn main() {
+    let matches = App::new("Print Log")
+        .version("1.0")
+        .author("Devin Schwab <dschwab@andrew.cmu.edu>")
+        .about("Prints an SSL RoboCup log.")
+        .arg(
+            Arg::with_name("LOG_FILE")
+                .help("Specifies log file that should be printed")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    let log_path = Path::new(matches.value_of("LOG_FILE").unwrap());
+    let reader = reader::LogReader::new_from_path(log_path).unwrap();
+    for message in reader {
+        println!("{:#?}", message);
+    }
+}
